e22f1e908d1637acf94149df1f2e3ef5df5c5510
Dhruv <dhruv27kapri@gmail.com>
2023-09-15 21:45:08 +0530

    5 - updated build files

--- api/index.js
+++ api/index.js
@@ -143,7 +143,7 @@ app.get("/post/:id", async (req, res) => {
 });
 
 // production script
-app.use(express.static("../client/build"));
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
 });
