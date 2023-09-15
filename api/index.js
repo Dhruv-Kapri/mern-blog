ecdc7b5d4f229bd1ef40f047d8bd2a436b2b48ad
Dhruv Kapri <103319551+Dhruv-Kapri@users.noreply.github.com>
2023-09-15 22:05:00 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const app = express();
 const salt = bcrypt.genSaltSync(10);
 const secret = sec;
 
-app.use(cors());
+app.use(cors({origin: FRONT_END_URL,credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
